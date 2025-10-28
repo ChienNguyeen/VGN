0039d4eb07fe1b5ad9288d59d76ea180f57674a1
ChienNguyeen <chiennguyenvan28012003@gmail.com>
2025-10-28 19:09:48 +0700

    update

--- train_CNN.py
+++ train_CNN.py
@@ -93,8 +93,8 @@ if __name__ == '__main__':
         train_set_txt_path = cfg.TRAIN.DRIVE_SET_TXT_PATH
         test_set_txt_path = cfg.TEST.DRIVE_SET_TXT_PATH
     elif args.dataset == 'STARE':
-        train_set_txt_path = "../data/STARE/list/train.txt"
-        test_set_txt_path = "../data/STARE/list/test.txt"
+        train_set_txt_path = "data/STARE/list/train.txt"
+        test_set_txt_path = "data/STARE/list/test.txt"
     elif args.dataset == 'CHASE_DB1':
         train_set_txt_path = cfg.TRAIN.CHASE_DB1_SET_TXT_PATH
         test_set_txt_path = cfg.TEST.CHASE_DB1_SET_TXT_PATH
